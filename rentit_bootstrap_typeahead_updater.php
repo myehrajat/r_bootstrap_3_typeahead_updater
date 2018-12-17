e25db8a6a51f3c12a761124f9054b4b56d54fed7
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-17 13:53:43 +0330

    bugfix: typeahead update need to update rentit_owl_carousel2_updater
    This Plugin requires the rentit_owl_carousel2_updater to be installed and active.

--- rentit_bootstrap_typeahead_updater.php
+++ rentit_bootstrap_typeahead_updater.php
@@ -11,37 +11,55 @@ Version: 1.0
 Author URI: https://ma.tt/
 Text Domain: Rentit_Bootstrap_3_Typeahead_Updater
 */
+
+
+
 //SOURCE v bootstrap 3:https://github.com/bassjobsen/Bootstrap-3-Typeahead  =>>>>> we use edited version a small edit
 //SOURCE v bootstrap 4:https://github.com/bassjobsen/typeahead.js-bootstrap4-css
+/**
+ * Detect plugin. For use on Front End only.
+ */
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-//
-/* add forntend */
-add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts', 400 );
-add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts', 400 );
-/* add backend */
-add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts', 400 );
-add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts', 400 );
-/******************************************/
-//Updating scripts
-/******************************************/
-function Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts() {
-	wp_deregister_script( 'renita_bootstrap-typeahead' );
+// check for plugin using plugin name
+if ( is_plugin_active( 'rentit_owl_carousel2_updater/rentit_owl_carousel2_updater.php' ) ) {
+  //plugin is activated
+	// to work correctly we need to to activate rentit_owl_carousel2_updater plugin
+	//
+	/* add forntend */
+	add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts', 400 );
+	add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts', 400 );
+	/* add backend */
+	add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts', 400 );
+	add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts', 400 );
+	/******************************************/
+	//Updating scripts
+	/******************************************/
+	function Rentit_Bootstrap_3_Typeahead_Updater_dequeue_scripts() {
+		wp_deregister_script( 'renita_bootstrap-typeahead' );
+	}
+	function Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts() {
+		wp_enqueue_script( 'renita_bootstrap-typeahead',plugins_url("Bootstrap-3-Typeahead/bootstrap3-typeahead-MYEDIT.js",__FILE__ ), array( 'renita_clustern' ), '4.0.2', true);
+	}
+	/*************************************
+	*************************************/
+	/* add backend */
+	add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles', 400 );
+	add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles', 400 );
+	/* add forntend */
+	/******************************************/
+	//Updating styles
+	/******************************************/
+	add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles', 400 );
+	add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles', 400 );
+	function Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles() {}
+	function Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles() {}
+}else{
+	add_action( 'admin_init', 'Rentit_Bootstrap_3_Typeahead_Updater_notice' );
+	function Rentit_Bootstrap_3_Typeahead_Updater_notice(){
+		 $plugin = plugin_basename( __FILE__ ); // 'myplugin'
+		 deactivate_plugins( $plugin ); // Deactivate 'myplugin'
+		?><div class="error"><p><?php 	esc_html_e( 'Sorry, but This Plugin requires the rentit_owl_carousel2_updater to be installed and active.', 'rentit' );
+	 ?></p></div><?php
+	}
 }
-function Rentit_Bootstrap_3_Typeahead_Updater_enqueue_scripts() {
-	wp_enqueue_script( 'renita_bootstrap-typeahead',plugins_url("Bootstrap-3-Typeahead/bootstrap3-typeahead-MYEDIT.js",__FILE__ ), array( 'renita_moment-with-locales' ), '4.0.2', true);
-}
-/*************************************
-*************************************/
-/* add backend */
-add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles', 400 );
-add_action( 'admin_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles', 400 );
-/* add forntend */
-/******************************************/
-//Updating styles
-/******************************************/
-add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles', 400 );
-add_action( 'wp_enqueue_scripts', 'Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles', 400 );
-function Rentit_Bootstrap_3_Typeahead_Updater_dequeue_styles() {}
-function Rentit_Bootstrap_3_Typeahead_Updater_enqueue_styles() {}
-
-
